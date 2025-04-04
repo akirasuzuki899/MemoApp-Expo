4430d99013282bd739118ce77819b9827a96d703
akira9238pg@gmail.com <a.suzuki@best-teacher-inc.com>
2025-04-04 12:54:06 +0900

    Update CircleButton component

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,4 +1,6 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
 import Header from '../../components/Header'
 import CircleButton from '../../components/CircleButton'
 
@@ -22,7 +24,9 @@ const Detail = (): JSX.Element => {
           買い物リスト
         </Text>
       </ScrollView>
-      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+      <CircleButton style={{ top: 160, bottom: 'auto' }}>
+        <Feather name='plus' size={40}/>
+      </CircleButton>
     </View>
   )
 }
