8563a1e0ab085113078f8473657b1aca89a7ed7e
鈴木輝 <akira9238pg@gmail.com>
2025-04-09 00:04:51 +0900

    Add create memo feature

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,22 +1,37 @@
-import { 
-  View, TextInput, StyleSheet , KeyboardAvoidingView
-} from 'react-native'
+import { View, TextInput, StyleSheet } from 'react-native'
 import { router } from 'expo-router'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
 
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
+import { auth, db } from '../../config'
 
-const handlePress = (): void => {
-  router.push('/memo/list')
+const handlePress = (memo: string): void => {
+  if (auth.currentUser === null) { return }
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+  addDoc(ref, {
+    bodyText: memo,
+    updatedAt: Timestamp.fromDate(new Date())
+  })
+    .then((docRef) => {
+      console.log('success', docRef.id)
+      router.back()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
 const Create = (): JSX.Element => {
+  const [memo, setMemo] = useState('')
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeyboardAvoidingView style={styles.container} >
       <View style={styles.inputContainer}>
-        <TextInput multiline style={styles.input} value={''}></TextInput>
+        <TextInput multiline style={styles.input} value={memo} onChangeText={setMemo} autoFocus></TextInput>
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton onPress={() => handlePress(memo)}>
         <Icon name="check" size={40} color="#fff" />
       </CircleButton>
     </KeyboardAvoidingView>
