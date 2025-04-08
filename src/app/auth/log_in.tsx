424b7b96e30c034d18b32f41d8c50bac3b158664
鈴木輝 <akira9238pg@gmail.com>
2025-04-08 19:42:53 +0900

    Add log_in sign_up feature

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,14 +1,24 @@
 import {
-  View, Text, TextInput, TouchableOpacity, StyleSheet
+  View, Text, TextInput, TouchableOpacity, StyleSheet, Alert
 } from 'react-native'
 import { Link, router } from 'expo-router'
 import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
+import { auth } from '../../config'
 import Button from '../../components/button'
 
-const handlePress = (): void => {
-  // ログイン
-  router.replace('/memo/list')
+const handlePress = (email: string, password: string): void => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log(userCredential.user.uid)
+      router.replace('/memo/list')
+    })
+    .catch((error) => {
+      const { code, message } = error
+      console.log(code, message)
+      Alert.alert('エラーが発生しました', message)
+    })
 }
 
 const LogIn = (): JSX.Element => {
@@ -21,7 +31,7 @@ const LogIn = (): JSX.Element => {
         <Text style={styles.title}>Log In</Text>
         <TextInput style={styles.input} value={email} onChangeText={setEmail} placeholder='Email address' autoCapitalize='none' keyboardType='email-address' textContentType='emailAddress'></TextInput>
         <TextInput style={styles.input} value={password} onChangeText={setPassword} placeholder='Password' autoCapitalize='none' secureTextEntry textContentType='password'></TextInput>
-        <Button label={'Submit'} onPress={handlePress} />
+        <Button label={'Submit'} onPress={() => { handlePress(email, password)}} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Not registered?</Text>
           <Link href='/auth/sign_up' asChild>
